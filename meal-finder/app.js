897dc640539ea69520c351895948a8453c5943e0
andriol <keli87aj@hotmail.com>
2021-12-23 18:01:50 +0200

    final commit

--- meal-finder/app.js
+++ meal-finder/app.js
@@ -1,13 +1,16 @@
 const search = document.getElementById("search");
 const form = document.getElementById("submit");
 const resultHeading = document.getElementById("result-heading");
-const searchBtn = document.querySelector(".search-btn");
 const singleMeal = document.getElementById("single-meal");
 const meals = document.getElementById("meals");
+const randomBtn = document.getElementById("random");
+
 form.addEventListener("submit", getRecipe);
+randomBtn.addEventListener("click", getRandom);
 
 function getRecipe(e) {
   e.preventDefault();
+  singleMeal.innerHTML = "";
   const searchValue = search.value;
   if (searchValue.trim()) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`)
@@ -35,6 +38,49 @@ function getRecipe(e) {
       });
     search.value = "";
   } else {
-    alert("Please enter a search term");
+    alert("Please fill in the search box.");
   }
+}
+
+function getRandom() {
+  resultHeading.innerHTML = "";
+  meals.innerHTML = "";
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((response) => response.json())
+    .then((res) => {
+      console.log(res);
+      const meal = res.meals[0];
+      recipeDOM(meal);
+    });
+}
+
+function recipeDOM(meal) {
+  const ingredients = [];
+  console.log(ingredients);
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+  singleMeal.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+      <div class="main">
+        <p>${meal.strInstructions}</p>
+        <h2>Ingredients</h2>
+        <ul>
+           ${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
+        </ul>
+      </div>
+    </div>
+  `;
 }
